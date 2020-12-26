9c7fc45e2706b1523669041df9b801c57bb68fd6
pauljay611 <pauljay611@gmail.com>
2020-12-26 19:18:08 +0800

    add getXXXByID

--- server/src/api/controllers.ts
+++ server/src/api/controllers.ts
@@ -8,12 +8,27 @@ export const getAllStores = async (req: Request, res: Response) => {
   res.status(200).json(stores);
 };
 
+export const getStoreByID = async (req: Request, res: Response) => {
+  const stores = await Store.findByPk(req.params.id);
+  res.status(200).json(stores);
+};
+
 export const getAllCoupons = async (req: Request, res: Response) => {
   const coupons = await Coupon.findAll();
   res.status(200).json(coupons);
 };
 
+export const getCouponByID = async (req: Request, res: Response) => {
+  const coupons = await Coupon.findByPk(req.params.id);
+  res.status(200).json(coupons);
+};
+
 export const getAllCategories = async (req: Request, res: Response) => {
   const categories = await Category.findAll();
+  res.status(200).json(categories);
+};
+
+export const getCategoryByID = async (req: Request, res: Response) => {
+  const categories = await Category.findByPk(req.params.id);
   res.status(200).json(categories);
 };
--- server/src/api/routes.ts
+++ server/src/api/routes.ts
@@ -3,14 +3,18 @@ import {
   getAllCategories,
   getAllCoupons,
   getAllStores,
+  getStoreByID,
 } from "../api/controllers";
 
 const router = express.Router();
 
 router.get("/stores", getAllStores);
+router.get("/store/:id", getStoreByID);
 
 router.get("/coupons", getAllCoupons);
+router.get("/coupons", getAllCoupons);
 
+router.get("/categories", getAllCategories);
 router.get("/categories", getAllCategories);
 
 export default router;
