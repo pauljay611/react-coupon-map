322023d44b26de8127fe199e35c690ba495cd378
pauljay611 <pauljay611@gmail.com>
2021-01-01 15:06:07 +0800

    add middleware and rootStore

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,6 +1,51 @@
-import { combineReducers } from "redux";
+import {
+  CombinedState,
+  combineReducers,
+  StateFromReducersMapObject,
+  compose,
+} from "redux";
+import { combineEpics, createEpicMiddleware } from "redux-observable";
+import { createStore, applyMiddleware } from "redux";
+import { getAllStoresEpic } from "./stores/epics";
 import storeReducer from "./stores/reducers";
+import { ActionsType } from "./stores/types";
 
-const rootReducer = combineReducers({ store: storeReducer });
+export type RootState = CombinedState<
+  StateFromReducersMapObject<typeof reducers>
+>;
 
-export type RootState = ReturnType<typeof rootReducer>;
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: Function;
+  }
+}
+
+const reducers = { store: storeReducer };
+
+const epicMiddleware = createEpicMiddleware<
+  ActionsType,
+  ActionsType,
+  RootState
+>();
+
+const rootReducer = combineReducers(reducers);
+
+const rootEpic = combineEpics(getAllStoresEpic);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Create store
+function configureStore(initialState?: RootState) {
+  // configure middlewares
+  const middlewares = [epicMiddleware];
+  // compose enhancers
+  const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+  // create store
+  return createStore(rootReducer, initialState, enhancer);
+}
+
+epicMiddleware.run(rootEpic);
+
+const store = configureStore();
+
+export default store;
--- client/src/store/stores/epics.ts
+++ client/src/store/stores/epics.ts
@@ -1,21 +1,23 @@
-import { Epic } from 'redux-observable'
-import { isOfType } from 'typesafe-actions'
-import { from } from 'rxjs'
-import { catchError, filter, map, mergeMap } from 'rxjs/operators'
+import { Epic } from "redux-observable";
+import { isOfType } from "typesafe-actions";
+import { from } from "rxjs";
+import { catchError, filter, map, mergeMap } from "rxjs/operators";
 
-import * as API from '../../services/api/store'
+import * as API from "../../services/api/store";
 
-import constants from './constants'
-import { ActionsType, StoreState } from './types'
-import actions from './actions'
+import constants from "./constants";
+import { ActionsType, StoreState } from "./types";
+import actions from "./actions";
+
+import { RootState } from "../index";
 
 export const getAllStoresEpic: Epic<
-	ActionsType,
-	ActionsType,
-	StoreState,
-	typeof API
+  ActionsType,
+  ActionsType,
+  RootState,
+  typeof API
 > = (action$, store, { getStoresAPI }) =>
-	action$.pipe(
-		filter(isOfType(constants.GET_ALL_STORES)),
-		mergeMap(() => from(getStoresAPI()).pipe(map(actions.setAllStores)))
-	)
+  action$.pipe(
+    filter(isOfType(constants.GET_ALL_STORES)),
+    mergeMap(() => from(getStoresAPI()).pipe(map(actions.setAllStores)))
+  );
