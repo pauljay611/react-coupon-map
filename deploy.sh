4ec9e89d8e32e1bb0d5c121497d87baba9e221a5
pauljay611 <pauljay611@gmail.com>
2021-01-24 17:03:39 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ yarn workspace server build
 
 echo "docker compose up"
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 docker-compose up -d
 
 echo "start db"
