d71b7e60f748a1fa6aaac4be7aee2b39ebc16867
Paul Yang <paulyang@17.media>
2021-01-25 18:13:33 +0800

    fix client

--- client/src/services/api/config.ts
+++ client/src/services/api/config.ts
@@ -1,13 +1,5 @@
 import axios from "axios";
 
-export const env = process.env.NODE_ENV as string;
-
-export const endPoint: Record<string, string> = {
-  production: "",
-  stage: "",
-  development: "http://localhost:8000",
-};
-
 export const api = axios.create({
-  baseURL: endPoint[env],
+  baseURL: process.env.API_ENDPOINT,
 });
